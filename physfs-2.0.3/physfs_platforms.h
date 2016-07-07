f81e901175dcddbc2aa51380e21ec79ff6221a1c
Gareth Nelson <gareth@garethnelson.com>
2016-07-07 18:54:06 +0100

    Begin zoidberging PhysFS

--- physfs-2.0.3/physfs_platforms.h
+++ physfs-2.0.3/physfs_platforms.h
@@ -5,45 +5,10 @@
 #error Do not include this header from your applications.
 #endif
 
-/*
- * These only define the platforms to determine which files in the platforms
- *  directory should be compiled. For example, technically BeOS can be called
- *  a "unix" system, but since it doesn't use unix.c, we don't define
- *  PHYSFS_PLATFORM_UNIX on that system.
- */
+// why not zoidberg?
+#define PHYSFS_PLATFORM_ZOIDBERG
+#define PHYSFS_NO_THREAD_SUPPORT
 
-#if (defined __HAIKU__)
-#  define PHYSFS_PLATFORM_HAIKU
-#  define PHYSFS_PLATFORM_BEOS
-#  define PHYSFS_PLATFORM_POSIX
-#elif ((defined __BEOS__) || (defined __beos__))
-#  define PHYSFS_PLATFORM_BEOS
-#  define PHYSFS_PLATFORM_POSIX
-#elif (defined _WIN32_WCE) || (defined _WIN64_WCE)
-#  define PHYSFS_PLATFORM_POCKETPC
-#elif (((defined _WIN32) || (defined _WIN64)) && (!defined __CYGWIN__))
-#  define PHYSFS_PLATFORM_WINDOWS
-#elif (defined OS2)
-#  define PHYSFS_PLATFORM_OS2
-#elif ((defined __MACH__) && (defined __APPLE__))
-/* To check if iphone or not, we need to include this file */
-# include <TargetConditionals.h> 
-# if ((TARGET_IPHONE_SIMULATOR) || (TARGET_OS_IPHONE))
-#    define PHYSFS_PLATFORM_UNIX
-#    define PHYSFS_PLATFORM_POSIX
-#    define PHYSFS_NO_CDROM_SUPPORT
-#  else
-#    define PHYSFS_PLATFORM_MACOSX
-#    define PHYSFS_PLATFORM_POSIX
-#  endif
-#elif defined(macintosh)
-#  error Classic Mac OS support was dropped from PhysicsFS 2.0. Move to OS X.
-#elif defined(unix) || defined(__unix__)
-#  define PHYSFS_PLATFORM_UNIX
-#  define PHYSFS_PLATFORM_POSIX
-#else
-#  error Unknown platform.
-#endif
 
 #endif  /* include-once blocker. */
 
