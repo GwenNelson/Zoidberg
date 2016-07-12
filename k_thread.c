c8fdf4be13a4f28d49da90b244ed70d40c249f20
Gareth Nelson <gareth@garethnelson.com>
2016-07-12 19:38:53 +0100

    dodgy scheduling

--- k_thread.c
+++ k_thread.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <setjmp.h>
 
 #include "kmsg.h"
 
@@ -10,26 +11,36 @@ extern EFI_BOOT_SERVICES *BS;
 EFI_EVENT timer_ev;
 
 typedef struct task_def_t {
-    uint64_t stack_pointer;
+    void* stack_pointer;
+    jmp_buf env;
 } task_def_t;
 
 task_def_t *tasks       = NULL;
-uint64_t    task_count  = 1;
+uint64_t    task_count  = 0;
 uint64_t    active_task = 0;
 
 void timer_func(EFI_EVENT Event, void *ctx) {
-     register void *esp __asm__ ("rsp");
-     if(tasks==NULL) {
-        tasks = malloc(sizeof(task_def_t));
-        tasks[0].stack_pointer = (uint64_t)esp;
+//     register void *esp __asm__ ("rsp");
+//     tasks[active_task].stack_pointer = esp;
+     if(setjmp(tasks[active_task].env)==0) {
+        active_task++;
+        if(active_task > task_count) active_task=0;
+//     esp = tasks[active_task].stack_pointer;
+        longjmp(tasks[active_task].env,1);
      }
-     tasks[active_task].stack_pointer = (uint64_t)esp;
-     active_task++;
-     if(active_task >= task_count) active_task=0;
-     esp = (void*)tasks[active_task].stack_pointer;
 }
 
+void init_task() {
+     task_count++;
+     tasks = realloc((void*)tasks,sizeof(task_def_t)*task_count);
+     active_task = task_count-1;
+}
+
+void thread_a();
+void thread_b();
+
 void thread_a() {
+     init_task();
 	int i=0;
      for(;;) {
 		i++;
@@ -38,6 +49,7 @@ void thread_a() {
 }
 
 void thread_b() {
+     init_task();
      for(;;) kprintf("Thread B\n");
 }
 
