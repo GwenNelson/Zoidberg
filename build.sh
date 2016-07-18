c22ccb63dda9812f68f6361a207ed92c6850042c
Gareth Nelson <gareth@garethnelson.com>
2016-07-18 14:05:59 +0100

    misc cleanups

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+echo if this does not run, try doing "source build.sh" instead of ./build.sh
+
 export PATH=~/bin/bin:$PWD/sdk:$PATH
 export WORKSPACE=`pwd`/../edk2
 export PACKAGES_PATH=$WORKSPACE:$PWD
 pushd $PWD
 cd $WORKSPACE
 . edksetup.sh
+popd
+
+mkdir -p pawn/source/compiler/build
+pushd pawn/source/compiler/build
+cmake ..
+make
 popd
