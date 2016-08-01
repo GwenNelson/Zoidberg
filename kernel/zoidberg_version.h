4ca6dcc5ac8445b246dd37cc2a5b4458a4103b99
Gareth Nelson <gareth@garethnelson.com>
2016-08-01 18:19:21 +0100

    imported elfload

--- kernel/zoidberg_version.h
+++ kernel/zoidberg_version.h
@@ -1,6 +1,6 @@
 #ifndef ZOIDBERG_VERSION_H
 #define ZOIDBERG_VERSION_H
 #define ZOIDBERG_VERSION "0.1"
-#define ZOIDBERG_BUILD  "7dfbfae"
-#define ZOIDBERG_BUILDDATE "Sun 31 Jul 15:15:53 BST 2016"
+#define ZOIDBERG_BUILD  "61f4cab"
+#define ZOIDBERG_BUILDDATE "Mon  1 Aug 18:19:13 BST 2016"
 #endif
