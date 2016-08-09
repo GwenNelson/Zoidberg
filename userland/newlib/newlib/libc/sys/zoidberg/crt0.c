2a679a60ce07f28c895c8a286b4b7b9795b7a2d5
Gareth Nelson <gareth@garethnelson.com>
2016-08-09 14:29:07 +0100

    remove sys_test() from crt0

--- userland/newlib/newlib/libc/sys/zoidberg/crt0.c
+++ userland/newlib/newlib/libc/sys/zoidberg/crt0.c
@@ -16,7 +16,6 @@ jmp_buf proc_start_env;
 
 EFI_STATUS
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-     sys_test(1,2,3);
      ST = SystemTable;
      BS = ST->BootServices;
      RT = ST->RuntimeServices;
